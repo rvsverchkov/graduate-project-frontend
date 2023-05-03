4f4f530e721ac3c1681ea00bb39793c73ba40b6a
Roman Sverchkov <rvsverchkov@gmail.com>
2023-05-03 21:54:28 +0300

    Исправление ссылок

--- src/router.js
+++ src/router.js
@@ -1,13 +1,25 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import startPage from './pages/startPage.vue';
 import signInPage from './pages/signInPage.vue';
 import signUpPage from './pages/signUpPage.vue';
 
 export default createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
-        { path: '/', component: startPage },
-        { path: '/signin', component: signInPage },
-        { path: '/signup', component: signUpPage },
+        {
+            name: 'Start',
+            path: '/', 
+            component: startPage 
+        },
+        {
+            name: 'SignIn',
+            path: '/signin', 
+            component: signInPage 
+        },
+        { 
+            name: 'SignUp',
+            path: '/signup', 
+            component: signUpPage 
+        },
     ] 
 })
